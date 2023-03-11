69d879729f4d60eeb2b36c9170cbd767e6ab1b11
ShengyenLin <shengyenlin0501@gmail.com>
2023-03-11 22:53:23 +0800

    update hw1

---
+++ hw1/README.md
@@ -0,0 +1,19 @@
+# HW1 - Term extraction
+
+## Requirement
+Write a program to extract terms from a document.
+- Tokenization
+- Lowercasing everything
+- Stemming using Porter’s algorithm
+- Stopword removal
+- Save the result as a txt file
+
+
+## Environment
+- Python: 3.6.8
+- Packages: nltk
+
+## Reproduce
+```bash
+python3 pa1.py
+```
---
+++ hw1/pa1.py
@@ -0,0 +1,92 @@
+from nltk.stem.porter import PorterStemmer
+
+def get_text():
+    '''
+    read content from txt
+    '''
+    f = open('content.txt')
+    text = []
+    for line in f.readlines():
+        text.append(line)
+    f.close()
+    return text
+
+def tokenize_text(text):
+    '''
+    去除換行符號、切開並將一些字還原
+    '''
+    replace_dictionary = {"'s": " is", "'ve": " have", "'re": " are"}
+    text_tokenized = []
+    for sentence in text:
+        sentence = sentence.strip()
+        for key, value in replace_dictionary.items():
+            sentence = sentence.replace(key, value)
+        words = sentence.split(' ')
+        for word in words:
+            text_tokenized.append(word)
+    return text_tokenized 
+      
+def delete_delimiters(text_tokenized):
+    '''
+    刪除標點符號
+    '''
+    delimiter = [',', "."]
+    text_final = []
+    for token in text_tokenized:
+        word = ""
+        for element in token:
+            if element not in delimiter:
+                word += element
+        word = word.lower()
+        text_final.append(word)
+    return text_final
+
+def lowercase_words(text_tokenized):
+    '''
+    將字母全部縮成小寫
+    '''
+    text_tokenized = [word.lower() for word in text_tokenized]
+    return text_tokenized
+
+def stemming_word(text_tokenized):
+    '''
+    Stemming using Porter’s algorithm
+    '''
+    porter_stemmer = PorterStemmer()
+    text_tokenized = [porter_stemmer.stem(word) for word in text_tokenized]
+    return text_tokenized
+    
+def remove_stopwords(text_tokenized):
+    '''
+    Stopword removal
+    '''
+    f = open('stopwords.txt')
+    stopwords = []
+    for stopword in f.readlines():
+        stopwords.append(stopword.strip())
+    f.close()
+    text_final = [word for word in text_tokenized if word not in stopwords]
+    return text_final
+
+def text_preprocessing(text_tokenized):
+    text_tokenized = tokenize_text(text)
+    text_no_del = delete_delimiters(text_tokenized)
+    text_lower = lowercase_words(text_no_del)
+    text_stemmed = stemming_word(text_lower)
+    text_final = remove_stopwords(text_stemmed)
+    return text_final
+
+
+def save_result(text_final):
+    '''
+    Save the result as a txt file
+    '''
+    with open('result.txt', 'w') as f:
+        for item in text_final:
+            f.write("%s\n" % item)
+
+#需要執行的部分            
+text = get_text()
+text_final = text_preprocessing(text)
+save_result(text_final)
+
